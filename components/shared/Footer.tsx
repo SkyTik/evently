225bd07611faea207dd3345c766f09d23bf4ad90
vunh <vunh@ahamove.com>
2024-01-23 14:58:25 +0700

    add mongoose and models User, Event

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gasp-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>2024 Evently. All Rights Reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ lib/database/models/user.mode.ts
@@ -0,0 +1,14 @@
+import { Model, models, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  photo: { type: String, required: true },
+});
+
+const User = models.User ?? new Model("User", UserSchema);
+
+export default User;
